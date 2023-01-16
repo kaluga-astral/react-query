af34c31edfa2db99f7db48e01d0abdde5d86ad42
Andrey Potyomkin <and_tem@icloud.com>
2023-01-16 17:34:43 +0300

    chore(UIKIT-521): Поправлен конфиг для тестов

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { createConfig } = require('@astral/commitlint-config');
 
-module.exports = createConfig({ scopes: packagesNames, ticketPrefix: 'UIKIT' });
+module.exports = createConfig({ ticketPrefix: 'UIKIT' });
--- package/vitest.config.ts
+++ package/vitest.config.ts
@@ -1,19 +1,12 @@
 /// <reference types="vitest" />
 
-import path from 'path';
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
-export const createVitestConfig = ({ useJsDOM }: { useJsDOM: boolean }) =>
-  defineConfig({
-    plugins: [react()],
-    test: {
-      globals: true,
-      environment: useJsDOM ? 'jsdom' : 'node',
-      setupFiles: useJsDOM
-        ? path.resolve(__dirname, 'package', 'vitest.setup.js')
-        : undefined,
-      testTimeout: 3000,
-    },
-  });
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    testTimeout: 3000,
+  },
+});
